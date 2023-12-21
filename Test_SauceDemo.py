d1c9172b5829481b0fd5c74e053ce0517f5c34cd
Asus <Asus@DESKTOP-6HHUABE>
2023-12-21 14:34:03 +0300

    webdriverwait & actionChains

--- Test_SauceDemo.py
+++ Test_SauceDemo.py
@@ -2,26 +2,54 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+from selenium.webdriver.support.wait import WebDriverWait #ilgili driverı bekleten yapı
+from selenium.webdriver.support import expected_conditions as ec #beklenen koşullar
+from selenium.webdriver.common.action_chains import ActionChains 
 
 class Test_SauceDemo:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.saucedemo.com")
+        self.driver.maximize_window() #ekranı büyütür
+
     def test_invalid_login(self):
-        driver = webdriver.Chrome()
-        driver.get("https://www.saucedemo.com")
-        driver.maximize_window() #ekranı büyütür
-        sleep(5)
-        usernameInput = driver.find_element(By.ID,"user-name")
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        #usernameInput = driver.find_element(By.ID,"user-name")
         usernameInput.send_keys("locked_out_user")
-        sleep(5)
-        passwordInput = driver.find_element(By.ID,"password")
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        #passwordInput = driver.find_element(By.ID,"password")
         passwordInput.send_keys("secret_sauce")
-        sleep(5)
-        loginButton = driver.find_element(By.ID,"login-button")
+        loginButton = self.driver.find_element(By.ID,"login-button")
         loginButton.click()
-        sleep(5)
-        errorMessage = driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
         testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
         print(f"TEST SONUCU: {testResult}")
 
+    def valid_login(self):
+        self.driver.get("https://www.saucedemo.com")
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput.send_keys("standard_user")
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput.send_keys("secret_sauce")
+        loginButton = self.driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        #anasayfaya ulaşıp ulaşamadığımın kanıtı
+        #headerLogo = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='header_container']/div[1]/div[2]/div")))
+        #testResult = headerLogo.text == "Swag Labs"
+        self.driver.execute_script("window.scrollTo(0,500)") #penceremin scrolunu aşağıya indiriyor,verdiğim koordinatlarla birlikte
+        addToCart = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")))
+        addToCart.click()
+        remove = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='remove-test.allthethings()-t-shirt-(red)']")))
+        testResult = remove.text == "Remove"
+        print(f"TEST SONUCU: {testResult}")
+        sleep(5)
+        
+        """ addToCart = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")))
+        actionChains = ActionChains(self.driver)
+        actionChains.move_to_element(addToCart) #butonun olduğu yere sayfayı taşı
+        actionChains.click()
+        actionChains.perform() #depolanan tüm fonksiyonları çalıştırır """
 
 testClass = Test_SauceDemo()
 testClass.test_invalid_login()
+testClass.valid_login()
